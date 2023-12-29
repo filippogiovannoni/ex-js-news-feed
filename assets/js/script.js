6e303cbb3e88bb889a0ad5d512479842c33cd298
filippogiovannoni <filippogiovannonii@gmail.com>
2023-12-29 10:57:09 +0100

    add functions and filtering options for tag

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ const newsList = [
         id: '2',
         title: 'Esplorando le profondità marine: il mistero degli abissi',
         content: 'Esplorando le profondità marine: il mistero degli abissi',
-        tags: ['viaggi', 'geo'],
+        tags: ['viaggi'],
         author: 'Fabio Mari',
         published: '2023-03-14',
         image: './assets/img/deep-sea.jpg alt="deep-sea"'
@@ -27,7 +27,7 @@ const newsList = [
         id: '3',
         title: 'Viaggio culinario: alla ricerca dei sapori perduti',
         content: 'Esplorazione di tradizioni culinarie dimenticate e la ricerca di sapori autentici.',
-        tags: ['cucina'],
+        tags: ['cucina', 'geo'],
         author: 'Marta Bianchi',
         published: '2023-04-20',
         image: './assets/img/kitchen-food.jpg alt="kitchen-food"'
@@ -52,79 +52,127 @@ newsList.forEach(news => {
 
     // Se il valore del secondo tag è indefinito
     if (news.tags[1] === undefined) {
-        
+
         // Al posto di undefined non ci sarà scritto niente
         news.tags[1] = '';
     }
- 
-    // Salvo in una variabile l'elemento dove inserire ogni news
-    const newsWrapperEl = document.querySelector('.news')
-
-    // Salvo in una variabile ogni card tramite la funzione
-    newsCardEl = generateCard(news)
-
-    // All'interno del wrapper vengono stampate le card
-    newsWrapperEl.innerHTML += newsCardEl
-    
 })
-    
-// Salvo in una variabile tutti gli span
-const spans = document.querySelectorAll('span')
-    
-    // Itero all'interno degli span e coloro ogni span in base al tag
-    spans.forEach(span => {
-        if (span.textContent.includes('cucina')) {
-            span.classList.add('bg_purple')
-        }
-        else if (span.textContent.includes('geo')) {
-            span.classList.add('bg-success')
-        }
-        else if (span.textContent.includes('viaggi')) {
-            span.style.backgroundColor = 'red'
-        }
-        else if (span.textContent.includes('arte')) {
-            span.classList.add('bg-warning')
-        }
-        else if (span.textContent.includes('tech')) {
-            span.style.backgroundColor = 'blue'
-        }
+
+// Salvo in una variabile l'elemento dove inserire ogni news
+const newsWrapperEl = document.querySelector('.news')
+
+// Stampo in pagina all'interno del wrapper tutte le news con una funzione
+renderNews(newsList, newsWrapperEl);
+// Coloro tramite la funzione tutti i tag
+colorTag();
+
+// Salvo in una variabile il select
+const selectTagsEl = document.getElementById('tags');
+
+// Al variare del valore all'interno del select viene scatenata la funzione
+selectTagsEl.addEventListener('change', function (e) {
+
+    // Seleziono il wrapper dove inserire le card
+    const newsWrapperEl = document.querySelector('.news');
+
+    // Svuoto il wrapper
+    newsWrapperEl.innerHTML = '';
+
+    // Salvo in un array le news filtrate in base al tag selezionato
+    const filteredNews = newsList.filter(news => {
+        return (news.tags).includes(e.target.value) || e.target.value === 'allTags';
     })
-    
-    
+
+    // Inserisco tramite la funzione l'array di news filtrate all'interno del wrapper
+    renderNews(filteredNews, newsWrapperEl);
+    // Coloro tramite la funzione i tag delle news filtrate
+    colorTag();
+})
 
 
+/**
+ * Generate the markup for a news card
+ * @param {object} news The news object
+ * @returns object
+ */
 function generateCard(news) {
-        
-        const cardMarkup = `
-    <div class="article my-3 p-3">
+
+    const cardMarkup = `
+        <div class="card-${news.id} my-3 p-3">
         <div class="title d-flex justify-content-between">
-            <h1 class="text-start fs-3 mb-0">${news.title}</h1>
-            <i class="btn border border-0 fa-solid fa-bookmark ms-5 fs-4"></i>
+        <h1 class="text-start fs-3 mb-0">${news.title}</h1>
+        <i class="btn border border-0 fa-regular fa-bookmark ms-5 fs-4"></i>
         </div>
-        <h6 class="text-start mb-0">pubblicato da ${news.author}</h6>
-        <span class="fs_12">in data ${formatDate(news.published)}</span>
-        <h6 class="d-block my-2">${news.content}</h6>
-        <img class="pb-3" width="100%" src=${news.image}>
-        <div class="tags">
-            <span class="badge p-2">${news.tags[0]}</span>
-            <span class="badge p-2">${news.tags[1]}</span>
-        </div>
-    </div>`
+            <h6 class="text-start mb-0">pubblicato da ${news.author}</h6>
+            <span class="fs_12">in data ${formatDate(news.published)}</span>
+            <h6 class="d-block my-2">${news.content}</h6>
+            <img class="pb-3" width="100%" src=${news.image}>
+            <div class="tags">
+                <span class="badge p-2">${news.tags[0]}</span>
+                <span class="badge p-2">${news.tags[1]}</span>
+            </div>
+        </div>`
 
     return cardMarkup
 
-    
+
 }
 
+/**
+ * Renders the list of news into the dom element
+ * @param {Array} newsList The list of news
+ * @param {object} domElement The node where append the news list
+*/
+function renderNews(newsList, domElement) {
 
+    newsList.forEach(news => {
+
+        // Salvo in una variabile ogni card tramite la funzione
+        newsCardEl = generateCard(news);
+
+        // All'interno del wrapper vengono stampate le card
+        domElement.insertAdjacentHTML('beforeend', newsCardEl);
+
+    })
+}
+
+/**
+ * Color the all tags with specific colors
+ */
+function colorTag() {
+    // Salvo in una variabile tutti gli span
+    const spans = document.querySelectorAll('span');
+
+    // Itero all'interno degli span e coloro ogni span in base al tag
+    spans.forEach(span => {
+        if (span.textContent.includes('cucina')) {
+            span.classList.add('bg_purple');
+        }
+        else if (span.textContent.includes('geo')) {
+            span.classList.add('bg-success');
+        }
+        else if (span.textContent.includes('viaggi')) {
+            span.style.backgroundColor = 'red';
+        }
+        else if (span.textContent.includes('arte')) {
+            span.classList.add('bg-warning');
+        }
+        else if (span.textContent.includes('tech')) {
+            span.style.backgroundColor = 'blue';
+        }
+    })
+}
+
+/**
+ * Format the date from the American format to the Italian one
+ * @param {string} date 
+ * @returns string
+ */
 function formatDate(dateInput) {
-    
-    const dataSplitted = dateInput.split('-')
-    
-    const dataOutput = `${dataSplitted[2]}/${dataSplitted[1]}/${dataSplitted[0]}`
 
-    return dataOutput
+    const dataSplitted = dateInput.split('-');
+
+    const dataOutput = `${dataSplitted[2]}/${dataSplitted[1]}/${dataSplitted[0]}`;
+
+    return dataOutput;
 }
-
-
-
